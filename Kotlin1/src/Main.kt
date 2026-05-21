50252b76d5e2e9397b31edaec11845ba4aec28d2
Chanwoo1124 <lgw22774@gmail.com>
2026-05-21 22:10:51 +0900

    첫 번째 커밋: Kotlin 학습 프로젝트 초기화

---
+++ Kotlin1/src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var number : Long = 5
+    val number2 : Long = 10
+    println(number2)
+
+
+}
