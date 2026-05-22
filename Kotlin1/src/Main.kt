1c631be931a58255e6a8ed8c2dbd74237c3fba54
Chanwoo1124 <lgw22774@gmail.com>
2026-05-22 23:44:16 +0900

    Kotlin 기초 복습: 문자열 처리, nullable 타입, Elvis 연산자 실습

--- Kotlin1/src/Main.kt
+++ Kotlin1/src/Main.kt
@@ -6,8 +6,27 @@ fun main() {
     val money2 = JavaMoney(110L)
 
     if (money > money2){
-        println("더 큽니다 ${money}")
+        println("더 큽니qwe다 ${money}")
     }
+
+    var a = 1
+    val b = 2
+
+
+    val chan : String = "찬우"
+
+    val ch = chan[1]
+    println(ch)
+
+    val chanwoo = """
+        이찬우
+        이찬우
+        이찬우
+        
+    """.trimIndent()
+
+    println(chanwoo)
+
 
 
 }
@@ -16,4 +35,12 @@ fun printAgeIfPerson(obj : Any?){
     val person = obj as? Person
     println(person?.name)
 }
+
+fun cal(a : String? , b : String?) : String {
+    println(a?.length?: return "하이")
+
+    return "성공"
+}
+
+
 
